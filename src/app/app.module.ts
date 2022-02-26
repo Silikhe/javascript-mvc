4e30487279696fbba49f0337e12fdc19f59f92b9
silikhe <silaswans@gmail.com>
2022-02-26 08:18:55 +0300

    feature(view): add angular material for ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FormsModule } from '@angular/forms';
+// import { MatDialogModule } from '@angular/material/dialog';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersComponent,
-    NavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UsersComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
