d45c7d2348532440ae6ce19db611282d82f00757
silikhe <silaswans@gmail.com>
2022-02-27 02:37:59 +0300

    refactoring api to use promises

--- src/app/shared/sevice.service.ts
+++ src/app/shared/sevice.service.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-// import { environment } from 'environments/environment';
+import {environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SeviceService {
-  baer_url = ;
+  // let baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
+
+  newsRequest() {
+    interface apiRespose {
+      name: string;
+      description: string;
+      imageUrl: string;
+    }
+
+    let promise = new Promise((resolve, reject)=>{
+      this.http.get<apiRespose>(environment.baseUrl)..toPromise().then(response =>{
+        this.name = 
+      })
+    })
+  }
   // const httpOptions = {
   //   headers: new HttpHeaders({
   //     'Content-Type':  'application/json',
   //     Authorization: 'my-auth-token'
   //   })
   // };
-  
 
   // getUsers() {
 
@@ -27,28 +40,28 @@ export class SeviceService {
   //   });
   // }
 
-  postNews(data: any) {
-    console.log(data);
-    return this.http
-      .post<any>(`${this.baer_url}/news`, data)
-      .pipe(map((res) => res));
-  }
+  // postNews(data: any) {
+  //   console.log(data);
+  //   return this.http
+  //     .post<any>(`${this.baer_url}/news`, data)
+  //     .pipe(map((res) => res));
+  // }
 
-  getNews() {
-    return this.http.get<any>(`${this.baer_url}/news`).pipe(
-      map((res) => {
-        console.log(res);
-        return res;
-      })
-    );
-  }
+  // getNews() {
+  //   return this.http.get<any>(`${this.baer_url}/news`).pipe(
+  //     map((res) => {
+  //       console.log(res);
+  //       return res;
+  //     })
+  //   );
+  // }
 
-  getUsers() {
-    return this.http.get<any>(`${this.baer_url}/users`).pipe(
-      map((res) => {
-        console.log(res);
-        return res;
-      })
-    );
-  }
+  // getUsers() {
+  //   return this.http.get<any>(`${this.baer_url}/users`).pipe(
+  //     map((res) => {
+  //       console.log(res);
+  //       return res;
+  //     })
+  //   );
+  // }
 }
